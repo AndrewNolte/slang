d628bd10e2fabffa7fe31e852eb20d61f0ddaf3f
Andrew Nolte <anolte@hudson-trading.com>
2025-09-02 18:05:47 -0400

    AllGenerateBranches and AllowInvalidTop

--- source/ast/symbols/ParameterBuilder.cpp
+++ source/ast/symbols/ParameterBuilder.cpp
@@ -229,7 +229,12 @@ const ParameterSymbolBase& ParameterBuilder::createParam(
             }
             else if (param->isPortParam() && !tt.getTypeSyntax() &&
                      (decl.hasSyntax || !decl.givenType)) {
-                reportError(*param);
+                if (useInvalidForMissing) {
+                    tt.setType(comp.getErrorType());
+                }
+                else {
+                    reportError(*param);
+                }
             }
         }
 
@@ -307,7 +312,12 @@ const ParameterSymbolBase& ParameterBuilder::createParam(
                 }
             }
             else if (param->isPortParam() && !declType.getInitializerSyntax()) {
-                reportError(*param);
+                if (useInvalidForMissing) {
+                    param->setValue(comp, nullptr, /* needsCoercion */ false);
+                }
+                else {
+                    reportError(*param);
+                }
             }
         }
 
