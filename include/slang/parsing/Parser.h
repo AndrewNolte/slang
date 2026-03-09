5d54668d70eb8c11e138b14ee1ccb9397ec2f019
Andrew Nolte <anolte@hudson-trading.com>
2026-03-08 22:02:40 -0400

    Parser: add getOpenDelims()

--- include/slang/parsing/Parser.h
+++ include/slang/parsing/Parser.h
@@ -171,6 +171,8 @@ public:
     /// Gets the current set of metadata collected during parsing.
     ParserMetadata&& getMetadata();
 
+    using ParserBase::getOpenDelims;
+
 private:
     using ExpressionOptions = detail::ExpressionOptions;
     using NameOptions = detail::NameOptions;
